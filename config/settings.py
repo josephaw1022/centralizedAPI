6709965a7c0b1faa0e16396f6fcb4ab2d0893e6e
josephaw1022 <josephsims1@gmail.com>
2021-06-30 19:12:08 -0400

    fixed allowed host

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0s5bh8n^+wbmi-!phj9r25o4l8^$!*t(c%$)9xr3ott3fa1$wc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", "https://joe-whiteaker-drf-blog.herokuapp.com"]
 
 
 # Application definition
