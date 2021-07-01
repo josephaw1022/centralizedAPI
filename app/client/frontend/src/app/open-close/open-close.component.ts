801a5e689eb0e1fb556d64d39de3ddc5c0f5548e
josephaw1022 <josephsims1@gmail.com>
2021-07-01 17:00:13 -0400

    add navbar

--- app/client/frontend/src/app/open-close/open-close.component.ts
+++ app/client/frontend/src/app/open-close/open-close.component.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import * as $ from 'jquery';
 @Component({
   selector: 'app-open-close',
   templateUrl: './open-close.component.html',
   styleUrls: ['./open-close.component.scss'],
 })
-export class OpenCloseComponent {
-  show = false;
+export class OpenCloseComponent implements OnInit {
   startBracket = '{';
   endBracket = '}';
-  codeValue = `\`
-    <h1> {{message}} </h1> 
-  \``;
+  codeValue = `\`<h1> {{message}} </h1>\` `;
 
   space = '...';
+
+  showCircle = false;
+
+  ngOnInit() {}
 }
