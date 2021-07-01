15988faa9d684278607ff5f5fb9cba9521be894d
josephaw1022 <josephsims1@gmail.com>
2021-07-01 15:59:52 -0400

    new frontend site

--- app/client/frontend/src/app/app.module.ts
+++ app/client/frontend/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OpenCloseComponent } from './open-close/open-close.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [AppComponent, OpenCloseComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- app/client/frontend/src/app/open-close/open-close.component.ts
+++ app/client/frontend/src/app/open-close/open-close.component.ts
@@ -1,44 +1,17 @@
-import { AfterContentInit, Component, OnInit } from '@angular/core';
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-  // ...
-} from '@angular/animations';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-open-close',
   templateUrl: './open-close.component.html',
   styleUrls: ['./open-close.component.scss'],
-  animations: [
-    trigger('popOverState', [
-      state(
-        'show',
-        style({
-          opacity: 1,
-        })
-      ),
-      state(
-        'hide',
-        style({
-          opacity: 0,
-        })
-      ),
-      transition('shows => hide', animate('600ms ease-out')),
-      transition('hide => show', animate('100ms ease-in')),
-    ]),
-  ],
 })
 export class OpenCloseComponent {
   show = false;
+  startBracket = '{';
+  endBracket = '}';
+  codeValue = `\`
+    <h1> {{message}} </h1> 
+  \``;
 
-  get stateName(): string {
-    return this.show ? 'show' : 'hide';
-  }
-
-  toggle() {
-    this.show = !this.show;
-  }
+  space = '...';
 }
