811e0dbe7893eeb4b66e2fdd134af42d8c49913b
josephaw1022 <josephsims1@gmail.com>
2021-07-01 23:18:04 -0400

    removed admin

--- app/api/article/admin.py
+++ app/api/article/admin.py
@@ -1,10 +1,10 @@
-from django.contrib import admin
-from .models import Article
-# Register your models here.
+# from django.contrib import admin
+# from .models import Article
+# # Register your models here.
 
 
-class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['title', 'content', 'date', 'imageUrl']
+# class ArticleAdmin(admin.ModelAdmin):
+#     list_display = ['title', 'content', 'date', 'imageUrl']
 
 
-admin.site.register(Article, ArticleAdmin)
+# admin.site.register(Article, ArticleAdmin)
--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,6 @@ from django.urls import path
 from django.urls.conf import include
 from rest_framework.schemas import get_schema_view
 from rest_framework.documentation import include_docs_urls
-
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
