a8b7197e65cf2b272a7ef0df7fa5deec810be42c
josephaw1022 <josephsims1@gmail.com>
2021-07-01 20:45:41 -0400

    testing out postgressql db

--- config/components/database.py
+++ config/components/database.py
@@ -7,7 +7,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+
+
+        'USER': 'ftsteavhddpkdm',
+
+        'PASSWORD': 'ee12a3bfd61946cf1625cd69af9f24be1840896faae4ebe235ab589d9031ed1c',
+
+        'HOST': 'ec2-34-202-54-225.compute-1.amazonaws.com',
+
+        'PORT': '5432',
+
     }
 }
