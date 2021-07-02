77797e21d19f56bfe4bed2c6a6ed13659af6468e
josephaw1022 <josephsims1@gmail.com>
2021-07-01 20:48:51 -0400

    changed drf config

--- config/components/drf.py
+++ config/components/drf.py
@@ -1,6 +1,4 @@
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema', 
-    'DEFAULT_AUTUHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication'
-    ]
+    
 }
