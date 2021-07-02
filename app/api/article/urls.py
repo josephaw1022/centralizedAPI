b8d642b93a17c6cdc8e26a6e7dda1c7bba01ed65
josephaw1022 <josephsims1@gmail.com>
2021-07-01 21:14:27 -0400

    edited register

--- app/api/article/urls.py
+++ app/api/article/urls.py
@@ -3,6 +3,6 @@
 from rest_framework import routers
 from .views import ArticleViews
 router = routers.SimpleRouter()
-router.register('article', ArticleViews, name='article')
+router.register('article', ArticleViews)
 
 urlpatterns = router.urls
