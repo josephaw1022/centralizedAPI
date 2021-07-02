4033b0b00b933fc87129a482ea52992b4043ac2a
josephaw1022 <josephsims1@gmail.com>
2021-07-01 22:05:14 -0400

    added name to serializer

--- app/api/article/serializers.py
+++ app/api/article/serializers.py
@@ -7,4 +7,5 @@ from .models import Article
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ['id', 'title', 'content', 'imageUrl', 'date', 'active']
+        name = "article"
+        fields = ["id", "title", "content", "date", "active"]
