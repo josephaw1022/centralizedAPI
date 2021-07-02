193c66b2fcd78237c7011293df0a514a6351df8a
josephaw1022 <josephsims1@gmail.com>
2021-07-01 20:55:57 -0400

    changed procfile some

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: gunicorn config.wsgi --log-file - --log-level debug
+manage.py makemigrations
 manage.py migrate
--- app/api/article/urls.py
+++ app/api/article/urls.py
@@ -1,6 +1,6 @@
 
 
-from rest_framework import routers, urlpatterns
+from rest_framework import routers
 from .views import ArticleViews
 router = routers.SimpleRouter()
 
